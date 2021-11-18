46507daaf1ebc5d734f1b1e70f37a84069dd1099
rsneal01 <rsneal01@gmail.com>
2021-11-18 10:22:24 -0800

    commit tree ce90e2afee1c30e99b8edafe604c4655662ee2cc

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '' );
+  }
+
+  static titleize(str){
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let newArray = str.split(" ")
+    for (let n=0; n < newArray.length; n++){
+      if ( n == 0 ) {
+        result.push( this.capitalize( newArray[ n ] ) )
+      } else {
+      if (exceptions.includes(newArray[n])){
+        result.push(newArray[n])
+      } else {
+        result.push(this.capitalize(newArray[n]));
+      }
+    }
+  }
+    return result.join(" ");
 }
+}
+  
+
+
+
+
+
+done()
